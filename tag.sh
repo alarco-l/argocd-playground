b823956e6d7a1229ab6752a83ddc9f58cd6e3a26
alarco-l <contact@alarcon.ludovic.com>
2020-03-31 12:02:24 +0200

    update tag script

--- tag.sh
+++ tag.sh
@@ -4,7 +4,7 @@ set -e
 
 for RESULT in $(grep -rl $1- .)
 do
-  sed -i 's/'$1'/'$2'/g' $RESULT
+  sed -i 's/\('$1'-\)\(.*\)/'$2'/g' $RESULT
   echo "Tag version now set to '$2' in $RESULT"
   git add $RESULT
 done
